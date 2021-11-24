2b0194ca1ed00e6baaaf4fd34526e548e28f3653
Yesenia Urdiales <Yeseurdiales726@gmail.com>
2021-11-24 10:02:19 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -34,23 +34,37 @@ number = 10;
 function add5() {
   return (number += 5);
 }
+number++
 
 function divideBy3() {
   return (number /= 3);
 }
+number /3 ; //=> 3
 
 divideBy3(); //=> 3.3333333333333335
+number/3; //=> 3.3333333333333
 
 add5(); //=> 8.333333333333334
-
+5+3.33334 //=> 8.333333333333334
 // reset number
 number = 10;
 
 add5(); //=> 15
 
+number+= 5; //=> 15
+
 divideBy3(); //=> 5
 
+number/3//=> 5
+
+dividedBy3(15)//=> 5
+
+number/3(15)//=> 5
+
 parseInt("2", 10); //=> 2
+
 parseInt("2.2222", 10);
+
 parseInt("nonsense!", 10);
+
 parseFloat("80.123999"); // 80.123999
